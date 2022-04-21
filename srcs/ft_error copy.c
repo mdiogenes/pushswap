a1c7314b31b965610c11c2a1b98aaad4bf5cc24e
msoler-e <mdiogenes@gmail.com>
2022-04-21 09:10:22 +0200

    pujat visualitzador

---
+++ srcs/ft_error copy.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_error.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: msoler-e <msoler-e@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 09:59:19 by msoler-e          #+#    #+#             */
+/*   Updated: 2022/03/18 11:05:53 by msoler-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_error(char *cnt, int error, t_gstk **resum, int argc)
+{
+	if (error == 2)
+		ft_putstr_fd(cnt, error);
+	if (error == 0)
+		ft_putstr_fd(cnt, error);
+	if (error == 1)
+		ft_putstr_fd(cnt, error);
+	ft_free(resum, argc);
+	exit (error);
+}
